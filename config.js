ffd1b7bbf8d07d5e73cd581cd01786cb05995bbd
ChiLong0402 <91748486+ChiLong0402@users.noreply.github.com>
2021-10-01 23:22:57 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,18 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
+    introTitle: 'Ngọc Phương',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
+    Đằng sau chữ yêu đây là thương,
+    btnIntro: 'Đi kiếm ny thoi :>>',
+    title: 'Toy đã iu em khi thấy nụ cười ấy 🙆🏿‍♂💞  ',
+    desc: 'Iu nhao dc khom =))) ',
+    btnYes: 'Iu nha :))',
     btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
+    question: 'Còn chờ gì mà k check ib nè 😘 ',
     btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    mess: 'Hết dịch ẵm đi chơi dới tui nha 🙆🏿‍♂🙆🏿‍♂',
     btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    messLink: 'https://github.com/chilong/Confess-Crush'
 }
